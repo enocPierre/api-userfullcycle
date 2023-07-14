d161f72130da3026b669d24b9b07ec8056b102c4
enocPierre <enoc.pierre@mercadolivre.com>
2023-07-14 20:46:02 -0300

    add create, findById, update, delete

--- apis/internal/infra/database/interfaces.go
+++ apis/internal/infra/database/interfaces.go
@@ -1,8 +1,18 @@
 package database
 
-import "github.com/goexpert/apis/internal/entity"
+import (
+	"github.com/goexpert/apis/internal/entity"
+)
 
 type UserInterface interface {
 	Creatte(user *entity.User) error
 	FindByEmail(email string) (*entity.User, error)
+}
+
+type PriductInterface interface {
+	Create(product *entity.Product) error
+	FindAll(page, limit int, sort string) ([]entity.Product, error)
+	FindById(id string) (*entity.Product, error)
+	Update(product *entity.Product) error
+	delete(id string) error
 }
