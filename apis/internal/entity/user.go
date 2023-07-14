708bf97fcca2fd103e670c476b45a4cfc421291d
enocPierre <enoc.pierre@mercadolivre.com>
2023-07-13 22:28:20 -0300

    add user

---
+++ apis/internal/entity/user.go
@@ -0,0 +1,31 @@
+package entity
+
+import (
+	"github.com/goexpert/apis/pkg/entity"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type User struct {
+	ID       entity.ID `joson:id`
+	Name     string    `joson:name`
+	Email    string    `joson:email`
+	Password string    `joson:password`
+}
+
+func NewUser(name, email, password string) (*User, error) {
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return nil, err
+	}
+	return &User{
+		ID:       entity.NewId(),
+		Name:     name,
+		Email:    email,
+		Password: string(hash),
+	}, nil
+}
+
+func (u *User) ValidatePassWord(password string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(u.Password), []byte(password))
+	return err == nil
+}
